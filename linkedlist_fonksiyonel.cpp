c23d5085497437cbb27b372f3a96d9dafd583fdf
mahammad <msxiyev@gmail.com>
2016-09-12 15:25:23 +0400

    Linkedlist fonksiyonel örneği:Display ve sona ekleme işlemleri

---
+++ linkedlist_fonksiyonel.cpp
@@ -0,0 +1,48 @@
+/* NOTES:
+  listeler sequential(sıralı erişim), diziler ramdom
+
+*/
+#include "stdio.h"
+#include "stdlib.h"
+
+struct Node{
+  int data;
+  Node *next;
+};
+
+typedef Node node;
+
+
+//fonksiyon prototipleri
+void Display(node *toor);
+void AddLast(node *toor, int value);
+//main
+int main(int argc, char const *argv[]) {
+  node *root;
+  root=(node *)malloc(sizeof(node)); //hafizada node boyutunda root için hüçre oluştur.
+  root->next=NULL;
+  root->data=16;
+
+  //listenin sonuna 3 eleman eklemek
+  for (int i = 1; i < 9; i++) {
+    AddLast(root,i);
+  }
+  Display(root);
+  return 0;
+}
+//Display
+void Display(node *toor){
+  while (toor != NULL) {
+    printf("-->%d", toor->data);
+    toor=toor->next;
+  }
+}
+//Listenin sonun eleman ekle
+void AddLast(node *toor, int value) {
+  while (toor->next != NULL) {
+    toor=toor->next;
+  }
+  toor->next= (node *)malloc(sizeof(node));
+  toor->next->data= value;
+  toor->next->next=NULL;
+}
